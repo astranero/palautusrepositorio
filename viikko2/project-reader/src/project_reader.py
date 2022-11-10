9d7c7e2d2d515bf0b2ac9edbaf236d1b401f8122
astranero <esa.taheresa@gmail.com>
2022-11-10 12:00:21 +0200

    tehtävä 2 tehty

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import tomli
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,11 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
+        toml_dict = tomli.loads(content)
         print(content)
-
+        name = toml_dict["tool"]["poetry"]["name"]
+        description = toml_dict["tool"]["poetry"]["description"]
+        dependencies = toml_dict["tool"]["poetry"]["dependencies"]
+        dev_dependencies = toml_dict["tool"]["poetry"]["dev-dependencies"]
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, dependencies, dev_dependencies)
