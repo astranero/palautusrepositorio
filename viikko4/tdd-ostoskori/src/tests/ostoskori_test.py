034c55d3790b934a2b6b26ab377ac351720cdd66
astranero <esa.taheresa@gmail.com>
2022-11-21 22:36:03 +0200

    Testit menivat lapi eli kahden saman tuotteen lisayksen jalkeen ostoskori sisalsi yhden ostoksen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -65,3 +65,9 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()
         self.assertEqual(len(ostos), 2)
 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_yksi_ostos(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()
+        self.assertEqual(len(ostos), 1)
