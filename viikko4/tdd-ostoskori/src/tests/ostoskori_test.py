ccea151ff8616c6e1e3da67270128aae983cb3da
astranero <esa.taheresa@gmail.com>
2022-11-21 22:33:29 +0200

    TDD Kahden eri tuotteen lisaamisen jalkeen ostoskorissa on kaksi ostosta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -46,3 +46,22 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+        nimi = ostos.tuotteen_nimi()
+        lukumaara = ostos.lukumaara()
+        self.assertEqual(nimi, maito.nimi())
+        self.assertEqual(lukumaara, 1)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        keksi = Tuote("Keksi", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(keksi)
+        ostos = self.kori.ostokset()
+        self.assertEqual(len(ostos), 2)
+
