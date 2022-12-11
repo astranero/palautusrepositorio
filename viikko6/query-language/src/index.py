160ae7804f733b6be4cca3da2b7f189f97b0ae7f
astranero <esa.taheresa@gmail.com>
2022-12-11 22:13:16 +0200

    Viikko 6 ratkaistu

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,18 +1,58 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+
+
+class QueryBuilder:
+
+    def __init__(self, all=All()):
+        self._matchers = all
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._matchers, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._matchers, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._matchers, HasFewerThan(value, attr)))
+
+    def oneOf(self, query1, query2):
+        return QueryBuilder(Or(query1, query2))
+
+    def build(self):
+        return self._matchers
+
 
 def main():
-    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+    query = QueryBuilder()
+    matcher = (query
+               .playsIn("NYR")
+               .hasAtLeast(
+                   10, "goals")
+               .hasFewerThan(20, "goals")
+               .build())
+
+    m1 = (
+        query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
     )
 
+    m2 = (
+        query
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
     for player in stats.matches(matcher):
         print(player)
 
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,13 +1,54 @@
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
-    
+
     def matches(self, player):
         for matcher in self._matchers:
             if not matcher.matches(player):
                 return False
-        
         return True
+
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return False
+        return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        result = False
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                result = True
+        return result
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+        return player_value < self._value
+
+
+class All:
+    def __init__(self):
+        pass
+
+    def matches(self, player):
+        return True
+
 
 class PlaysIn:
     def __init__(self, team):
@@ -16,6 +57,7 @@ class PlaysIn:
     def matches(self, player):
         return player.team == self._team
 
+
 class HasAtLeast:
     def __init__(self, value, attr):
         self._value = value
@@ -23,5 +65,4 @@ class HasAtLeast:
 
     def matches(self, player):
         player_value = getattr(player, self._attr)
-
         return player_value >= self._value
